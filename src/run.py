72356a8f873d05216360a9e4d60bcae27bcb1c1f
Ryota Kusunoki <ryotakusunoki@hotmail.com>
2020-09-06 02:07:26 +0900

    argparse

--- src/run.py
+++ src/run.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import time
 from multiprocessing import Pool
@@ -7,13 +8,12 @@ import pandas as pd
 from cond import *
 from data import plot_graph
 from eval import energy, torque
-from pso_gauss import PSO as PSO_GAUSS
-from pso_power import PSO as PSO_POWER
+from pso import PSO_GAUSS, PSO_POWER
 from rk4 import RK4
 from traj import cycloid
 
 
-def train(v):
+def train(v, process):
     v["datadir"] = f"data/te{v['TE']}_se{v['SE']}/{v['mode']}/"
 
     if v["mode"] == "power":
@@ -21,11 +21,11 @@ def train(v):
     elif v["mode"] == "gauss_n2":
         optim = PSO_GAUSS(v)
 
-    # 1回だけ計算
-    # results = [optim.compute(0)]
-    # 10回計算
-    p = Pool(4)
-    results = p.map(optim.compute, range(10))
+    if process == "single":
+        results = [optim.compute(0)]
+    elif process == "parallel":
+        p = Pool(4)
+        results = p.map(optim.compute, range(10))
 
     for i, res in enumerate(results):
         np.savetxt(
@@ -33,24 +33,32 @@ def train(v):
             res,
             delimiter=",",
         )
+        print(
+            "Parameter saved at \n    "
+            + v["datadir"]
+            + f"param/{i}_param_pso_{v['mode']}_te{v['TE']}_se{v['SE']}.csv!\n",
+            end="\n\n",
+        )
 
 
-def test(v):
+def test(v, process):
     """学習したパラメーターからテストを実行
     """
     v["datadir"] = f"data/te{v['TE']}_se{v['SE']}/{v['mode']}/"
 
+    count = 1 if process == "single" else 10
+
     sum_energy = 0
-    for i in range(10):
+    for i in range(count):
         v["i"] = i
 
-        param_path = (
-            v["datadir"] + f"param/{v['i']}_param_pso_{v['mode']}_te{v['TE']}_se{v['SE']}.csv"
-        )
+        param_path = v["datadir"] + f"param/{i}_param_pso_{v['mode']}_te{v['TE']}_se{v['SE']}.csv"
         with open(param_path) as file:
             reader = csv.reader(file)
             a = np.array([float(row[0]) for row in reader])
-        print(f"Param: {a}")
+
+        print(f"Parameter loaded from \n    {param_path}!\n")
+        print(f"a = {a}\n")
 
         S = cycloid(a, v)
 
@@ -71,9 +79,7 @@ def test(v):
                 "w2": w2,
             }
         )
-        df.to_csv(
-            v["datadir"] + f"output/{v['i']}_output_pso_{v['mode']}_te{v['TE']}_se{v['SE']}.csv"
-        )
+        df.to_csv(v["datadir"] + f"output/{i}_output_pso_{v['mode']}_te{v['TE']}_se{v['SE']}.csv")
 
         sum_energy += energy(df["trq"], df["θ"])
         print(f'{i}_ene: {energy(df["trq"], df["θ"])}')
@@ -86,11 +92,25 @@ def test(v):
 if __name__ == "__main__":
     start = time.time()
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mode", help="Train or Test")
+    parser.add_argument("process", help="Single or Parallel")
+    args = parser.parse_args()
+
     v = {
         "mode": "gauss_n2",
         "TE": "1.0",
         "SE": "135",
     }
-    test(v)
+    if args.mode == "train":
+        print("#################")
+        print("# Train Running #")
+        print("#################", end="\n\n")
+        train(v, args.process)
+    elif args.mode == "test":
+        print("################")
+        print("# Test Running #")
+        print("################", end="\n\n")
+        test(v, args.process)
 
     print(f"Elapsed time: {time.time()-start}")
