081fcf8bc6f2460222c63c961202f9b11d967eac
Ryota Kusunoki <ryotakusunoki@hotmail.com>
2020-09-07 23:54:43 +0900

    Changed the contents of print.

--- src/run.py
+++ src/run.py
@@ -57,8 +57,8 @@ def test(v, process):
             reader = csv.reader(file)
             a = np.array([float(row[0]) for row in reader])
 
-        print(f"Parameter loaded from \n    {param_path}!\n")
-        print(f"a = {a}\n")
+        print(f'{i}. Parameter{i} loaded from \n    "{param_path}"!')
+        print(f"    a: {a}")
 
         S = cycloid(a, v)
 
@@ -82,35 +82,39 @@ def test(v, process):
         df.to_csv(v["datadir"] + f"output/{i}_output_pso_{v['mode']}_te{v['TE']}_se{v['SE']}.csv")
 
         sum_energy += energy(df["trq"], df["θ"])
-        print(f'{i}_ene: {energy(df["trq"], df["θ"])}')
+        print(f'    {i}_ene: {energy(df["trq"], df["θ"])}\n')
 
-        plot_graph(df, v, isShow=True)
+        plot_graph(df, v)
 
-    print(f"Average of energy: {sum_energy / 10}")
+    print(f"Average of energy: {sum_energy / count}\n")
 
 
 if __name__ == "__main__":
     start = time.time()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("mode", help="Train or Test")
-    parser.add_argument("process", help="Single or Parallel")
+    parser.add_argument("mode", help="Train or Test.")
+    parser.add_argument("process", help="Single or Parallel.")
+    parser.add_argument("--plot", help="Plot the graph or not.")
     args = parser.parse_args()
 
     v = {
         "mode": "gauss_n2",
-        "TE": "1.0",
-        "SE": "135",
+        "TE": str(TE),
+        "SE": str(int(np.rad2deg(SE))),
+        "isShow": bool(args.plot) if args.plot else False,
     }
     if args.mode == "train":
-        print("#################")
-        print("# Train Running #")
-        print("#################", end="\n\n")
+        print("#####################")
+        print("#   Train Running   #")
+        print("#####################")
+        print("TE = {}[s], SE = {}[deg]".format(str(TE), str(int(np.rad2deg(SE)))), end="\n\n")
         train(v, args.process)
     elif args.mode == "test":
-        print("################")
-        print("# Test Running #")
-        print("################", end="\n\n")
+        print("####################")
+        print("#   Test Running   #")
+        print("####################")
+        print("TE = {}[s], SE = {}[deg]".format(str(TE), str(int(np.rad2deg(SE)))), end="\n\n")
         test(v, args.process)
 
     print(f"Elapsed time: {time.time()-start}")
