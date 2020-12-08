9b030b6e0d563435aa656c53f08abe4c629f9ef7
sonokr <ryotakusunoki@hotmail.com>
2020-12-08 17:40:49 +0900

    batch.sh

---
+++ src/batch.sh
@@ -0,0 +1,4 @@
+poetry run python src/run.py src/config/nsga2_f1_r10k_gauss6_p6_te09_se90.yaml --line True
+poetry run python src/run.py src/config/nsga2_f1_r10k_power_p6_te09_se90.yaml --line True
+poetry run python src/run.py src/config/nsga2_f2_r10k_gauss6_p6_te09_se90.yaml --line True
+poetry run python src/run.py src/config/nsga2_f2_r10k_power_p6_te09_se90.yaml --line True
