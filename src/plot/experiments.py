f36a741dd5b723b7d82e0347e322aae9cc3e7449
sonokr <ryotakusunoki@hotmail.com>
2020-12-12 23:37:19 +0900

    rename

--- src/plot/experiments.py
+++ src/plot/experiments.py
@@ -12,6 +12,8 @@ Nrk = round(Tend / dt)
 if __name__ == "__main__":
     # exp data
     df = pd.read_csv("data/2020-11-04/楠1104実験用データ/te0.8se45/ガウス関数/6/g_s45_t08.csv")
+    s_exp = np.array(df['"Model Root"/"EX_S2"'])
+    ds_exp = np.array(df['"Model Root"/"EX_V2"'])
     w1_exp = np.array(df['"Model Root"/"w1"'])
     w2_exp = np.array(df['"Model Root"/"w2"'])
     trq_exp = np.array(df['"Model Root"/"Trq"']) - 0.04
@@ -20,6 +22,8 @@ if __name__ == "__main__":
     df = pd.read_csv(
         "data/2020-11-04/楠1104実験用データ/te0.8se45/ガウス関数/6/0_3_output_pso_gauss_n6_te0.8_se45.csv"
     )
+    s_sim = np.array(df["θ"]) * np.rad2deg(1)
+    ds_sim = np.array(df["dθ"])
     w1_sim = np.array(df["w1"]) * np.rad2deg(1) / 2.7244
     w2_sim = np.array(df["w2"]) * np.rad2deg(1) / 2.7244
     trq_sim = np.array(df["trq"])
@@ -29,15 +33,21 @@ if __name__ == "__main__":
     # plot setting
     savedir = "data/plot/exp/"
 
-    mpl.rcParams["figure.figsize"] = [6.0, 3.0]
+    mpl.rcParams["figure.figsize"] = [6.0, 5.0]
     plt.rcParams["mathtext.fontset"] = "stix"
     plt.rcParams["mathtext.default"] = "default"
 
     for exp, sim, name, axis in zip(
-        [w1_exp, w2_exp, trq_exp],
-        [w1_sim, w2_sim, trq_sim],
-        ["w1", "w2", "trq"],
-        [r"\theta [deg]", r"\theta [deg]", r"\tau [J]"],
+        [s_exp, ds_exp, w1_exp, w2_exp, trq_exp],
+        [s_sim, ds_sim, w1_sim, w2_sim, trq_sim],
+        ["s", "ds", "w1", "w2", "trq"],
+        [
+            r"$\theta \rm{[deg]}$",
+            r"$d\theta \rm{[deg / s]}$",
+            r"$\theta \rm{[deg]}$",
+            r"$\theta \rm{[deg]}$",
+            r"$\tau \rm{[J]}$",
+        ],
     ):
         print(name)
 
@@ -47,8 +57,8 @@ if __name__ == "__main__":
         fig.subplots_adjust(bottom=0.2)
 
         # plot
-        ax.plot(t, exp[:1501], label="Experiment")
-        ax.plot(t, sim, label="Simulation")
+        ax.plot(t[:1001], exp[:1001], label="Experiment")
+        ax.plot(t[:1001], sim[:1001], label="Simulation")
 
         ax.set_ylabel(axis)
         ax.set_xlabel(r"$t [s]$")
